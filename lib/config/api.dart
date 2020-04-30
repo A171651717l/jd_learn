77a01e8ca753fce051ea9d52aef67f4df5d3b28b
zhuzhilong <171651717@qq.com>
2020-04-30 23:07:05 +0800

    1

---
+++ lib/config/api.dart
@@ -0,0 +1,14 @@
+class JdApi {
+  static const String BASE_URL = 'http://192.168.0.113:5001/api';
+  // 首页请求的json数据
+  static const String HOME_PAGE = BASE_URL + '/profile/homepage';
+  // 分类页的导航
+  static const String CATEGORY_NAV = BASE_URL + '/profiles/navigationLeft';
+  // 分类页的商品类目的json数据
+  static const String CATEGORY_CONTENT = BASE_URL + '/profiles/navigationRight';
+  // 返回的商品列表的json数据
+  static const String PRODUCTIONS_LIST = BASE_URL + '/profiles/productionsList';
+  // 返回的商品详情的json数据
+  static const String PRODUCTIONS_DETAIL =
+      BASE_URL + '/profiles/productionDetail';
+}
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,6 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:jd_learn/config/api.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key key}) : super(key: key);
@@ -10,11 +12,16 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    NetRequest();
     return Scaffold(
-      appBar: AppBar(
-        title: Text('home')
-      ),
+      appBar: AppBar(title: Text('home')),
       body: Container(),
     );
   }
+}
+
+NetRequest() async {
+  var dio = Dio();
+  Response response = await dio.get(JdApi.HOME_PAGE);
+  print(response.data);
 }
