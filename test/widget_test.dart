15ba58383ce0fd475138dfdcab1e2be752d09f64
zhuzhilong <171651717@qq.com>
2020-05-05 03:28:04 +0800

    websocket success

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,97 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
+import 'dart:convert';
+import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
+import 'dart:async';
 
-import 'package:jd_learn/main.dart';
+import 'package:flutter/services.dart';
+import 'package:better_socket/better_socket.dart';
+// import 'dart:convert' show utf8;
 
-void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+void main() => runApp(MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+class _MyAppState extends State<MyApp> {
+  String _platformVersion = 'Unknown';
+  @override
+  void initState() {
+    super.initState();
+    initPlatformState();
+  }
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });
+  // Platform messages are asynchronous, so we initialize in an async method.
+  Future<void> initPlatformState() async {
+    String platformVersion;
+    // Platform messages may fail, so we use a try/catch PlatformException.
+    try {
+      platformVersion = await BetterSocket.platformVersion;
+
+      //设置监听
+      BetterSocket.addListener(onOpen: (httpStatus, httpStatusMessage) {
+        print(
+            "onOpen---httpStatus:$httpStatus  httpStatusMessage:$httpStatusMessage");
+
+        // BetterSocket.sendMsg('hello');
+      }, onMessage: (message) {
+        print("onMessage---message:$message");
+      }, onClose: (code, reason, remote) {
+        print("onClose---code:$code  reason:$reason  remote:$remote");
+      }, onError: (message) {
+        print("onError---message:$message");
+      });
+      var headers = {
+        "origin": "wss://api.matrixone.io/coinsdata/api/MarketsList/"
+      };
+
+      BetterSocket.connentSocket(
+          "wss://api.matrixone.io/coinsdata/api/MarketsList/",
+          httpHeaders: headers,
+          trustAllHost: true);
+    } on PlatformException {
+      platformVersion = 'Failed to get platform version.';
+    }
+
+    // If the widget was removed from the tree while the asynchronous platform
+    // message was in flight, we want to discard the reply rather than calling
+    // setState to update our non-existent appearance.
+    if (!mounted) return;
+
+    setState(() {
+      _platformVersion = platformVersion;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Plugin example app'),
+        ),
+        body: Center(
+          child: FlatButton(
+            onPressed: () {
+              // BetterSocket.sendMsg('hello');
+              var data = {"market": "USDT", "exchange": "BN"};
+
+              // BetterSocket.sendMsg( data.toString());
+              BetterSocket.sendByteMsg(
+                  Uint8List.fromList(jsonEncode(data).codeUnits));
+            },
+            child: Text('Running on: $_platformVersion\n'),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    BetterSocket.close();
+    super.dispose();
+  }
 }
